f83c8e3e9b54b98ea87972cd360cae923391490a
Logon92 <you@example.com>
2024-03-04 17:33:33 +0100

    subir cambios

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import "./style.css";
+
+interface grupo {
+    nombre: string;
+    añoFundacion: number;
+    activo: boolean;
+    generoMusical: string;
+}
+
+const grupo1 = {
+    nombre: "The Beatles",
+    añoFundacion: 1960,
+    activo: true ,
+    generoMusical: "Pop Rock",
+};
+
+const grupo2 = {
+    nombre: "Queen",
+    añoFundacion: 1970,
+    activo: false ,
+    generoMusical: "Rock",
+};
+
+console.log(grupo1);
+console.log(grupo2);
+
+//Separador
+
+const estiloTitulo = "color: red; font-size: 18px"
+
+console.log("")
